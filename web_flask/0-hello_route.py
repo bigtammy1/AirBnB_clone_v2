cd9f997966b14a6a45dd2c16843ade1159760515
Tammy <bigtammy1@gmail.com>
2023-05-20 20:47:58 +0000

    Done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ from flask import Flask, render_template
 app = Flask("__name__")
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return render_template("10-hbnb_filters.html")
