e20acb8277e506548e07d5d3c6201d97b5be6541
Tammy <bigtammy1@gmail.com>
2023-05-21 01:00:30 +0000

    Done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,18 +2,18 @@
 """Starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
+    /airbnb-onepage/: Displays 'Hello HBNB!'
 """
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return render_template("10-hbnb_filters.html")
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000)
